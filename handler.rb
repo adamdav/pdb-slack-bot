1be17c1c16177debb9db98261f04213c38288b53
adamdav <adamdvdsn@gmail.com>
2019-11-12 12:22:17 -0800

    Handle empty string in request

--- handler.rb
+++ handler.rb
@@ -3,7 +3,19 @@ require 'uri'
 require 'net/http'
 
 def command(event:, context:)
-  cmd = Hash[URI.decode_www_form(event['body'])].fetch('text', 'help')
+  text = Hash[URI.decode_www_form(event['body'])]['text']
+
+  cmd = case text
+  when nil
+    "help"
+  else
+    if text.strip == ''
+      "help"
+    else
+      text.strip
+    end
+  end
+
   res = Net::HTTP.post URI(ENV['PUPPET_DEBUGGER_URL']), cmd, "Content-Type" => "application/json"
 
   {
